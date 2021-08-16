650e786a87669ed3902763aa689429eb8e6d17bd
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-16 18:52:28 -0400

    Update stage1.asm

--- bootloader/stage1.asm
+++ bootloader/stage1.asm
@@ -1,16 +1,34 @@
+section .boot
 bits 16
-org 0x7c00
-
+global boot
 boot:
 	mov ax, 0x2401
 	int 0x15
+
 	mov ax, 0x3
 	int 0x10
+
+	mov [disk],dl
+
+	mov ah, 0x2    ;read sectors
+	mov al, 6      ;sectors to read
+	mov ch, 0      ;cylinder idx
+	mov dh, 0      ;head idx
+	mov cl, 2      ;sector idx
+	mov dl, [disk] ;disk idx
+	mov bx, copy_target;target pointer
+	int 0x13
 	cli
 	lgdt [gdt_pointer]
 	mov eax, cr0
 	or eax,0x1
 	mov cr0, eax
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
 	jmp CODE_SEG:boot2
 gdt_start:
 	dq 0x0
@@ -32,18 +50,17 @@ gdt_end:
 gdt_pointer:
 	dw gdt_end - gdt_start
 	dd gdt_start
-
+disk:
+	db 0x0
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
+times 510 - ($-$$) db 0
+dw 0xaa55
+copy_target:
 bits 32
+	hello: db "Photon Bootloader                                                               Now Loading Kernel...",0
 boot2:
-	mov ax, DATA_SEG
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
 	mov esi,hello
 	mov ebx,0xb8000
 .loop:
@@ -55,8 +72,14 @@ boot2:
 	add ebx,2
 	jmp .loop
 halt:
+	mov esp,kernel_stack_top
+	extern kmain
+	call kmain
 	cli
 	hlt
-hello: db "Photon Bootloader                                                               Now Loading Kernel...",0
-times 510 - ($-$$) db 0
-dw 0xaa55
+
+section .bss
+align 4
+kernel_stack_bottom: equ $
+	resb 16384 ; 16 KB
+kernel_stack_top:
