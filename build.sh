3ea801c38088a50906a7508e4b109cd4d68e25bf
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-16 16:57:06 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-export DISPLAY=:0.0
+echo "Now building..."
+git pull
 nasm -f bin bootloader/stage1.asm -o stage1
+echo "Building complete, now running in QEMU."
+export DISPLAY=:0.0
 qemu-system-i386 -no-reboot stage1
