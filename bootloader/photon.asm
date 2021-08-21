087995cc09d661b1d5167465a993bc4e8c7f36d9
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 13:45:45 -0400

    Update photon.asm

--- bootloader/photon.asm
+++ bootloader/photon.asm
@@ -1,21 +1,19 @@
-;
-; A simple boot sector that prints a message to the screen using a BIOS routine.
-;
-mov ah, 0x0e ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
-mov al,’H’
-int 0x10
-mov al,’e’
-int 0x10
-mov al,’l’
-int 0x10
-mov al,’l’
-int 0x10
-mov al,’o’
-int 0x10
-jmp $ ; Jump to the current address ( i.e. forever ).
-;
-; Padding and magic BIOS number.
-;
-times 510 -( $ - $$ ) db 0 ; Pad the boot sector out with zeros
-dw 0xaa55 ; Last two bytes form the magic number ,
-; so BIOS knows we are a boot sector.
+[bits 16]    ; use 16 bits
+[org 0x7c00] ; sets the start address
+
+init: 
+  mov si, msg  ; loads the address of "msg" into SI register
+  mov ah, 0x0e ; sets AH to 0xe (function teletype)
+print_char:
+  lodsb     ; loads the current byte from SI into AL and increments the address in SI
+  cmp al, 0 ; compares AL to zero
+  je done   ; if AL == 0, jump to "done"
+  int 0x10  ; print to screen using function 0xe of interrupt 0x10
+  jmp print_char ; repeat with next byte
+done:
+  hlt ; stop execution
+
+msg: db "Hello world!", 0 ; we need to explicitely put the zero byte here
+
+times 510-($-$$) db 0           ; fill the output file with zeroes until 510 bytes are full
+dw 0xaa55                       ; magic number that tells the BIOS this is bootable
