d8e85cdc113d72e2a04a816ef88119f567c7853b
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 13:38:25 -0400

    Update photon.asm

--- bootloader/photon.asm
+++ bootloader/photon.asm
@@ -1,85 +1,21 @@
-section .boot
-bits 16
-global boot
-boot:
-	mov ax, 0x2401
-	int 0x15
-
-	mov ax, 0x3
-	int 0x10
-
-	mov [disk],dl
-
-	mov ah, 0x2    ;read sectors
-	mov al, 32      ;sectors to read
-	mov ch, 0      ;cylinder idx
-	mov dh, 0      ;head idx
-	mov cl, 2      ;sector idx
-	mov dl, [disk] ;disk idx
-	mov bx, copy_target;target pointer
-	int 0x13
-	cli
-	lgdt [gdt_pointer]
-	mov eax, cr0
-	or eax,0x1
-	mov cr0, eax
-	mov ax, DATA_SEG
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	jmp CODE_SEG:boot2
-gdt_start:
-	dq 0x0
-gdt_code:
-	dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10011010b
-	db 11001111b
-	db 0x0
-gdt_data:
-	dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10010010b
-	db 11001111b
-	db 0x0
-gdt_end:
-gdt_pointer:
-	dw gdt_end - gdt_start
-	dd gdt_start
-disk:
-	db 0x0
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
-
-times 510 - ($-$$) db 0
-dw 0xaa55
-copy_target:
-bits 32
-	hello: db "Photon Bootloader                                                               Now Loading Kernel...",0
-boot2:
-	mov esi,hello
-	mov ebx,0xb8000
-.loop:
-	lodsb
-	or al,al
-	jz halt
-	or eax,0x0F00
-	mov word [ebx], ax
-	add ebx,2
-	jmp .loop
-halt:
-	mov esp,kernel_stack_top
-	extern start
-	call start
-	cli
-	hlt
-
-section .bss
-align 4
-kernel_stack_bottom: equ $
-	resb 2048 ; 2 KB
-kernel_stack_top:
+;
+; A simple boot sector that prints a message to the screen using a BIOS routine.
+;
+mov ah , 0 x0e ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
+mov al , ’H ’
+int 0 x10
+mov al , ’e ’
+int 0 x10
+mov al , ’l ’
+int 0 x10
+mov al , ’l ’
+int 0 x10
+mov al , ’o ’
+int 0 x10
+jmp $ ; Jump to the current address ( i.e. forever ).
+;
+; Padding and magic BIOS number.
+;
+times 510 -( $ - $$ ) db 0 ; Pad the boot sector out with zeros
+dw 0 xaa55 ; Last two bytes form the magic number ,
+; so BIOS knows we are a boot sector.
