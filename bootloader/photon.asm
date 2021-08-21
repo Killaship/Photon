f79359500de7c1d783d7587bdba2fe6a1e9931a0
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 13:43:04 -0400

    Update photon.asm

--- bootloader/photon.asm
+++ bootloader/photon.asm
@@ -1,17 +1,17 @@
 ;
 ; A simple boot sector that prints a message to the screen using a BIOS routine.
 ;
-mov ah , 0 x0e ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
-mov al , ’H ’
-int 0 x10
-mov al , ’e ’
-int 0 x10
-mov al , ’l ’
-int 0 x10
-mov al , ’l ’
-int 0 x10
-mov al , ’o ’
-int 0 x10
+mov ah, 0x0e ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
+mov al, ’H ’
+int 0x10
+mov al, ’e ’
+int 0x10
+mov al, ’l ’
+int 0x10
+mov al, ’l ’
+int 0x10
+mov al, ’o ’
+int 0x10
 jmp $ ; Jump to the current address ( i.e. forever ).
 ;
 ; Padding and magic BIOS number.
