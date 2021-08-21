0112287818ba2e339cae022fddcdaf614d004f28
Killaship <69988679+Killaship@users.noreply.github.com>
2021-08-21 13:48:34 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "Now building..."
 git pull
-nasm -f elf32 bootloader/photon.asm -o photon.o
-ld -m elf_i386 photon.o -o photon
+nasm -f bin bootloader/photon.asm -o photon
+
 echo "Building complete, now running in QEMU."
 export DISPLAY=:0.0
 qemu-system-i386 -fda photon
