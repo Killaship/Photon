91165c8a61f3933529dd291092af14a28b200c84
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-19 12:29:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git pull
 
-nasm -f bin bootloader/photon.asm -o photon.bin
-
-qemu-system-i386 -fda photon.bin -curses
+nasm -f bin bootloader/mbr.asm -o mbr.bin
+nasm -f bin bootloader/stage2.asm -o stage2.bin
+cat mbr.bin stage2.bin > os.bin
+qemu-system-i386 -fda os.bin.bin -curses
