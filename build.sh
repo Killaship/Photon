6a42e6b1b3834ea8ffa3e2bd1d2e63e50af2084f
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-19 18:07:10 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git pull
 
 nasm -f elf32 bootloader/mbr.asm -o mbr.o
-g++ -m32 bootloader/kmain.cpp mbr.o -o os.bin -nostdlib -ffreestanding -std=c++11 -mno-red-zone -fno-exceptions -nostdlib -fno-rtti -Wall -Wextra -Werror -T linker.ld
-qemu-system-i386 -fda os.bin -curses
+g++ -m64 bootloader/kmain.cpp mbr.o -o os.bin -nostdlib -ffreestanding -std=c++11 -mno-red-zone -fno-exceptions -nostdlib -fno-rtti -Wall -Wextra -Werror -T linker.ld
+qemu-system-x86_64 -fda os.bin -curses
