8c43c225093b13c343a30aa1082fdec41dc5abc5
Killaship <69988679+Killaship@users.noreply.github.com>
2022-01-25 15:56:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ git pull
 nasm -f bin bootloader/mbr.asm -o mbr.bin
 nasm -f bin bootloader/stage2.asm -o stage2.bin
 cat mbr.bin stage2.bin > os.bin
-qemu-system-i386 -fda os.bin -curses
+qemu-system-i386 -fda os.bin # -curses
