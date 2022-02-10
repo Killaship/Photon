cc11bd97e061a87e9fae79c5d44620b67b5049d9
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-09 20:55:48 -0500

    Update kentry.asm

--- bootloader/kentry.asm
+++ bootloader/kentry.asm
@@ -1,4 +1,18 @@
 bits 32
+
 extern kmain 
+extern keyboard_handler_main
+
+global keyboard_handler
+global loadprgm
+
+keyboard_handler:                 
+	call    keyboard_handler_main
+	iretd
+
+loadprgm:
+	jmp $ ; do later
+  
+  
 call kmain ; calls the kernel main function
 jmp $
