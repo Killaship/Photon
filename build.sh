8ea5874065b93922e10e7853adcc26f081825d63
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-09 21:12:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 git pull
 
 nasm -f bin bootloader/mbr.asm -o mbr.bin
-nasm -f bin bootloader/stage2.asm -o stage2.bin
-cat mbr.bin stage2.bin > os.bin
-qemu-system-i386 -fda os.bin # -curses
+nasm -felf bootloader/kentry.asm kasm.o
+
+gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -ffreestanding
+
+ld -m elf-i386 -o kernel.bin -Ttext 0x1000 kasm.o kc.o --oformat binary
+
+cat mbr.bin kernel.bin > os.bin
+
+qemu-system-i386 -fda os.bin -curses -rtc base=localtime
