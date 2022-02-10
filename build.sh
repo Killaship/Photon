6c71e4e9a78ab3060b3382fec3e1b7cf5277d533
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-09 21:35:15 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd -
 
 gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -ffreestanding
 
-ld -m elf_i386 -o kernel.bin -Ttext 0x1000 bootloader/kasm.o kc.o --oformat binary
+ld -m elf_i386 -o kernel.bin -Ttext 0x1000 bootloader/kasm.o kc.o --oformat binary --ignore-unresolved-symbol _GLOBAL_OFFSET_TABLE_
 
 cat bootloader/mbr.bin kernel.bin > os.bin
 
