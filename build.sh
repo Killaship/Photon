fae94370d3143aef7b9ce9ef2a55ebf348679598
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-09 21:27:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ cd -
 
 gcc -Wall -Wextra -fno-stack-protector -m32 -c kernel/kernel.c -o kc.o -ffreestanding
 
-ld -m elf_i386 -o kernel.bin -Ttext 0x1000 kasm.o kc.o --oformat binary
+ld -m elf_i386 -o kernel.bin -Ttext 0x1000 bootloader/kasm.o kc.o --oformat binary
 
-cat mbr.bin kernel.bin > os.bin
+cat bootloader/mbr.bin kernel.bin > os.bin
 
-#qemu-system-i386 -fda os.bin -curses -rtc base=localtime
+qemu-system-i386 -fda os.bin -curses -rtc base=localtime
